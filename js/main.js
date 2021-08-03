76792c796de555b4cab4607e0a0ee59de3829e77
hugolesati <hugolesati@hotmail.com>
2021-08-02 22:12:39 -0300

    Ejerciocio clase 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
-let hugo = 1
+// for (let i = 1; i <= 5; i++){
+//     if (i == 4) {
+//         break
+//     }
 
-console.log("hugo")
+//     let = ingresaNombre = prompt("Ingrese nombre " + i);
+
+//     alert("Turno N° " + i + "Nombre: " + ingresaNombre)
+// }
+
+
+
+let personaje = prompt("Ingresa un personaje");
+
+ while(personaje != "parar"){
+    switch (personaje) {
+        case "hermione":
+        alert("Hermione es un personaje de Harry Potter");
+        break;
+        case "Tony Stark":
+        alert("Tony Stark es un personaje de los Vengadores");
+        break;
+        case "Toretto":
+        alert("Toretto es un personaje de Rapidos y Furiosos")
+        break;    
+        case "Eren":
+        alert("Eren Jaeger es un personaje de Attack on Titan")
+        default:
+        alert("Ingresa un personaje por favor")
+        break;
+    }        
+   personaje = prompt("Ingrese un personaje") 
+ }
